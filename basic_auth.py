5bac7845fab9161a838740cb3fc89963593beeef
LarsBergqvist <lars.g.bergqvist@gmail.com>
2016-09-03 19:07:31 +0200

    Added ssl

--- basic_auth.py
+++ basic_auth.py
@@ -1,10 +1,10 @@
+#!/usr/bin/env python
 from flask import Flask, request, jsonify, render_template
-from functools import wraps
-import sys, getopt
+import functools, sys, getopt
 
 app = Flask(__name__)
 
-def check_auth(username, password):
+def ok_user_and_password(username, password):
     return username == app.config['USERNAME'] and password == app.config['PASSWORD']
 
 def authenticate():
@@ -12,35 +12,34 @@ def authenticate():
     resp = jsonify(message)
 
     resp.status_code = 401
-    resp.headers['WWW-Authenticate'] = 'Basic realm="Example"'
+    resp.headers['WWW-Authenticate'] = 'Basic realm="Main"'
 
     return resp
 
-
-def requires_auth(f):
-    @wraps(f)
+def requires_authentication(f):
+    @functools.wraps(f)
     def decorated(*args, **kwargs):
         auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
+        if not auth or not ok_user_and_password(auth.username, auth.password):
             return authenticate()
         return f(*args, **kwargs)
     return decorated
 
-@app.route('/secret/api')
-@requires_auth    
+@app.route('/api/secret')
+@requires_authentication    
 def api_secret():
     return jsonify(get_secret_message())
+
+@app.route('/')
+@requires_authentication
+def index():
+    return render_template('index.html', message=get_secret_message())
 
 def get_secret_message():
     return ("The secret messages are calling to me endlessly, " +
     "they call to me across the air, " +
     "the messages across the atmosphere, " +
     "they whisper in your ear, they're calling everywhere")
-
-@app.route('/secret')
-@requires_auth    
-def index():
-    return render_template('index.html', message=get_secret_message())
 
 def main(argv):
     user = ''
@@ -72,4 +71,6 @@ def printHelp():
     
 if __name__ == '__main__':
     main(sys.argv[1:])
-    app.run(debug=False)
+    context = ('ssl.cert', 'ssl.key')
+    app.run(host='0.0.0.0', port=80, ssl_context=context)    
+    
